613208e4aff8fcc7200f8699c8c1badfef540cef
unknown <sysadmin@christinmorton.com>
2024-04-06 10:48:19 -0700

    updating servies layout...

--- app/services/tile-setter/page.jsx
+++ app/services/tile-setter/page.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
+import yanProfilePic from "../../../public/images/Resized_Resized_20220817_124533_223826879431907.JPEG";
+import projectPic1 from "../../../public/images/Resized_Resized_20231109_125211_223389226286137.JPEG";
+import projectPic2 from "../../../public/images/Resized_Resized_20230815_102145_223622606178443.JPEG";
+import projectPic3 from "../../../public/images/stock/charlesdeluvio-zy0s5QJZTYo-unsplash.jpg";
+import projectPic4 from "../../../public/images/stock/pexels-jean-van-der-meulen-1457847.jpg";
+import projectPic5 from "../../../public/images/Resized_Resized_20231204_161849_223463781147931.JPEG";
+import projectPic6 from '../../../public/images/Resized_Resized_20240306_165443.JPEG'
+import headerPic from "../../../public/images/stock/pexels-luis-yanez-206172.jpg";
+
+// style={{backgroundImage: `url(/images/Resized_Resized_20240306_165443.JPEG)`}} 
+
 function TileSetter() {
   return (
     <main className="min-w-full">
-      <section className="min-w-full min-h-screen ">
-        <div className=" my-auto">
-          <h1 className="text-3xl font-semi-bold text-center lg:text-6xl lg:text-left text-picton-blue-500">
+      <section className="min-w-full min-h-screen">
+        <div className="min-h-[500px] flex flex-col items-center justify-evenly pt-28">
+          <h1 className="text-2xl font-semi-bold text-center lg:text-6xl lg:text-left text-picton-blue-500">
             Elevate Your Space with Expert Craftsmanship
           </h1>
 
@@ -21,50 +32,52 @@ function TileSetter() {
       </section>
       
 
-      <section className="min-h-[250px] bg-picton-blue-400">
+      <section className="min-h-[250px] bg-picton-blue-400 px-16 pt-12">
         <div className="">
-        <h2 className="text-3xl font-semi-bold text-center lg:text-6xl lg:text-left text-picton-blue-100"
->
+        <h2 className="text-3xl font-semi-bold text-center lg:text-6xl text-picton-blue-100">
            Why not choose professional tile setting?
         </h2>
-        <p className="max-w-md mx-auto text-sm text-center mb-4 text-white lg:text-lg lg:text-left lg:mt-0 lg:mx-0">
-          Proper tile setting is essential for achieving durable, long-lasting
-          results that enhance the beauty and functionality of your space.
-        </p>
-        <p className="max-w-md mx-auto text-sm text-center mb-4 text-white lg:text-lg lg:text-left lg:mt-0 lg:mx-0">
-          With professional expertise, you can ensure precise alignment,
-          seamless transitions, and superior craftsmanship that withstands the
-          test of time.
-        </p>
-        <p className="max-w-md mx-auto text-sm text-center  text-white lg:text-lg lg:text-left lg:mt-0 lg:mx-0">
-          Trusting your tile installation to an experienced professional like
-          Yan ensures peace of mind and stunning results that exceed your
-          expectations.
-        </p>
+        <div className="h-1/2 flex flex-col items-center justify-center p-6 space-y-4 lg:flex-row lg:h-[500px] lg:space-x-6">
+          <p className="max-w-md mx-auto text-sm text-center mb-4 text-white lg:text-lg lg:text-left lg:mt-0 lg:mx-0">
+            <span className="text-md font-extrabold">Proper tile setting</span> is essential for achieving durable, long-lasting
+            results that enhance the beauty and functionality of your space.
+          </p>
+          <p className="max-w-md mx-auto text-sm text-center mb-4 text-white lg:text-lg lg:text-left lg:mt-0 lg:mx-0">
+            With <span className="text-md font-extrabold">professional expertise</span>, you can ensure precise alignment,
+            seamless transitions, and superior craftsmanship that withstands the
+            test of time.
+          </p>
+          <p className="max-w-md mx-auto text-sm text-center  text-white lg:text-lg lg:text-left lg:mt-0 lg:mx-0">
+            Trusting your tile installation to an <span className="text-md font-extrabold">experienced professional</span> like
+            Yan ensures peace of mind and stunning results that exceed your
+            expectations.
+          </p>
+        </div>
         </div>
       </section>
 
-      <section className="min-w-full min-h-screen  bg-slate-200">
-        <div className="">
-        <h2 className="text-3xl font-semi-bold text-center lg:text-6xl lg:text-left text-picton-blue-500">
+      <section className="min-w-full min-h-screen  bg-slate-200 p-16">
+        <div className="px-12">
+        <h2 className="text-2xl font-semi-bold text-center lg:text-5xl lg:text-left text-picton-blue-500">
           Yan&apos;s Process: From Concept to Completion Heading: 
         </h2>
-
-        <h2 className="text-3xl font-semi-bold text-center lg:text-6xl lg:text-left text-picton-blue-500">The Yan Difference: A Proven Process for Success</h2>
         </div>
-        <div className="row-start-2 col-start-2">
+        <div className="h-[500px] flex flex-col items-center justify-center p-8 space-y-8 lg:flex-row space-x-8">
         <p className="max-w-md mx-auto text-lg text-center text-picton-blue-400 lg:text-xl lg:text-left lg:mt-0 lg:mx-0">
-          <span>Consultation:</span> Yan begins by listening to your vision, discussing design ideas, and assessing the scope of your project to create a tailored plan.
+          <span className="text-3xl font-serif font-extrabold text-gray-800 px-4">1.</span><span>Consultation:</span> Yan begins by listening to your vision, discussing design ideas, and assessing the scope of your project to create a tailored plan.
         </p>
         <p className="max-w-md mx-auto text-lg text-center text-picton-blue-400 lg:text-xl lg:text-left lg:mt-0 lg:mx-0">
-          <span>Preparation:</span> Proper surface preparation is crucial for ensuring a smooth and level substrate, laying the foundation for flawless tile installation.
+         <span className="text-3xl font-serif font-extrabold text-gray-800 px-4">2.</span><span>Preparation:</span> Proper surface preparation is crucial for ensuring a smooth and level substrate, laying the foundation for flawless tile installation.
         </p>
         <p className="max-w-md mx-auto text-lg text-center text-picton-blue-400 lg:text-xl lg:text-left lg:mt-0 lg:mx-0">
-          <span>Installation:</span> Using advanced techniques and premium materials, Yan meticulously lays each tile with precision and care, paying attention to every detail.
+         <span className="text-3xl font-serif font-extrabold text-gray-800 px-4">3.</span><span>Installation:</span> Using advanced techniques and premium materials, Yan meticulously lays each tile with precision and care, paying attention to every detail.
         </p>
         <p className="max-w-md mx-auto text-lg text-center text-picton-blue-400 lg:text-xl lg:text-left lg:mt-0 lg:mx-0">
-          <span>Finishing Touches:</span> Once the tiles are set, Yan applies grout, seals edges, and performs quality checks to ensure a flawless finish that exceeds your expectations.
+          <span className="text-3xl font-serif font-extrabold text-gray-800 px-4">4.</span><span>Finishing Touches:</span> Once the tiles are set, Yan applies grout, seals edges, and performs quality checks to ensure a flawless finish that exceeds your expectations.
         </p>
+        </div>
+        <div className="px-12">
+        <h2 className="text-xl font-semi-bold text-center lg:text-3xl lg:text-right text-picton-blue-500">The Yan Difference: A Proven Process for Success</h2>
         </div>
       </section>
 
